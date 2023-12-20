c3888ea4bb04f72a3e3a7d477b190b292033240e
Andrés Yépez Calderón <andresyepez966@gmail.com>
2023-12-20 23:15:40 +0100

    Update ensure_installed of Treesitter

--- plugins.lua
+++ plugins.lua
@@ -66,5 +66,49 @@ local plugins = {
       require "custom.configs.lspconfig"
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults 
+        "vim",
+        "lua",
+
+        -- web dev 
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+
+        -- high level 
+        "python",
+        "cpp",
+
+        -- low level
+        "c",
+
+        -- shell
+        "bash",
+
+        -- build
+        "cmake",
+        "make",
+
+        -- reporting
+        "latex",
+
+        -- database
+        "sql",
+
+        -- serialization
+        "json",
+        "yaml",
+        "toml",
+
+        -- regex
+        "regex",
+      },
+    },
+  },
 }
 return plugins
