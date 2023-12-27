a5deedc6627b15c4e69fbc93eb5fc2623d33ba5a
Andrés Yépez Calderón <andresyepez966@gmail.com>
2023-12-27 07:29:10 +0100

    Enable dapui controls for repl the correct way (so not hacky as before)

--- plugins.lua
+++ plugins.lua
@@ -29,9 +29,8 @@ local plugins = {
       require("core.utils").load_mappings("dap")
 
       -- Open and close repl when debug is initialiaze and terminated/exited
-      -- First open and close dapui so that its repl settings apply
-      require('dapui').open()
-      require('dapui').close()
+      -- First enable dapui controls for repl
+      require('dapui.controls').enable_controls(require('dapui').elements['repl'])
       local dap = require("dap")
       dap.listeners.after.event_initialized["dap_repl"] = function ()
         dap.repl.open()
