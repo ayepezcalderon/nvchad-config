823c1198729037a2b554c4f917bf300b3b209cf7
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-01-29 22:44:49 +0100

    Put colorcolumn auto-command under an augroup

--- init.lua
+++ init.lua
@@ -24,7 +24,9 @@ vim.api.nvim_create_user_command('ToggleIndentBlanklineContextStart', toggle_con
 
 
 ------------------------------------ autocommands ---------------------------------------
+vim.api.nvim_create_augroup('Colorcolumns', { clear = true })
 vim.api.nvim_create_autocmd({"BufRead", "BufNewFile", "BufEnter", "BufWinEnter"}, {
   pattern = {"*.py"},
+  group = 'Colorcolumns',
   callback = function() opt.colorcolumn = '88' end,
 })
