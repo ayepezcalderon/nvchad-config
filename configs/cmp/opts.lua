fab8edf0c13b487001efa7de8de7deaac2a55d62
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-03-09 13:42:19 +0100

    Fix my own bug regarding cmp-dap configuration

--- configs/cmp/opts.lua
+++ configs/cmp/opts.lua
@@ -1,10 +1,10 @@
 local opts = {
   enabled = function()
     local disabled = false
-    disabled = disabled or (vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt')
+    disabled = disabled or (vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt'
+        and not require("cmp_dap").is_dap_buffer())
     disabled = disabled or (vim.fn.reg_recording() ~= '')
     disabled = disabled or (vim.fn.reg_executing() ~= '')
-    disabled = disabled or require("cmp_dap").is_dap_buffer()
     return not disabled
   end,
 }
