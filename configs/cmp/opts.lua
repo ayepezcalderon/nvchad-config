693fbb5e788ed0ebd4ea612ddfeb8a5470234001
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-03-10 12:44:42 +0100

    Fix cmp-dap again... Current config only works because nvim-cmp loads on InsertEnter, and nvim-dap loads on filetype. Hence, nvim-dap loads first. THIS IS NOT ROBUST. cmp-dap should really be a dependency of nvim-cmp and not nvim-dap. Nvchad doesn't allow this. CONSIDER moving away from nvchad

--- configs/cmp/opts.lua
+++ configs/cmp/opts.lua
@@ -1,8 +1,13 @@
 local opts = {
   enabled = function()
     local disabled = false
-    disabled = disabled or (vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt'
-        and (package.loaded['cmp-dap'] and not require("cmp_dap").is_dap_buffer()))
+    if package.loaded['cmp_dap'] then
+      disabled = disabled or (vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt'
+          and not require("cmp_dap").is_dap_buffer())
+
+    else
+      disabled = disabled or (vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt')
+    end
     disabled = disabled or (vim.fn.reg_recording() ~= '')
     disabled = disabled or (vim.fn.reg_executing() ~= '')
     return not disabled
