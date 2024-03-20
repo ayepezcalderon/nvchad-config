4290f929a0a3ff233e27ea47b01b271ebf197fed
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-03-20 17:08:41 +0100

    Improve mappings for treesitter-text-objects

--- configs/treesitter-text-objects.lua
+++ configs/treesitter-text-objects.lua
@@ -13,7 +13,7 @@ M.opts = {
         -- You can use the capture groups defined in textobjects.scm
         ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
         ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
-        ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+        ["L="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
         ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
 
         ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
@@ -52,28 +52,28 @@ M.opts = {
       goto_next_start = {
         ["]f"] = { query = "@call.outer", desc = "Next function call start" },
         ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
-        ["]c"] = { query = "@class.outer", desc = "Next class start" },
+        ["]b"] = { query = "@class.outer", desc = "Next class start" },
         ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
         ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
       },
       goto_next_end = {
         ["]F"] = { query = "@call.outer", desc = "Next function call end" },
         ["]M"] = { query = "@function.outer", desc = "Next method/function def end" },
-        ["]C"] = { query = "@class.outer", desc = "Next class end" },
+        ["]B"] = { query = "@class.outer", desc = "Next class end" },
         ["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
         ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
       },
       goto_previous_start = {
         ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
         ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
-        ["[c"] = { query = "@class.outer", desc = "Prev class start" },
+        ["[b"] = { query = "@class.outer", desc = "Prev class start" },
         ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
         ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
       },
       goto_previous_end = {
         ["[F"] = { query = "@call.outer", desc = "Prev function call end" },
         ["[M"] = { query = "@function.outer", desc = "Prev method/function def end" },
-        ["[C"] = { query = "@class.outer", desc = "Prev class end" },
+        ["[B"] = { query = "@class.outer", desc = "Prev class end" },
         ["[I"] = { query = "@conditional.outer", desc = "Prev conditional end" },
         ["[L"] = { query = "@loop.outer", desc = "Prev loop end" },
       },
